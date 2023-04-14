ff962d3b3eece5722bc917ab33ba5ad61bf0e6ab
Speik <speik0102@gmail.com>
2023-04-14 23:04:13 +0400

    Page title improved

--- src/app/components/page-title/page-title.component.ts
+++ src/app/components/page-title/page-title.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 
 type PageTitlePosition = 'center' | 'left' | 'right';
-const DEFAULT_LABEL = '<No label>';
 
 @Component({
   selector: 'app-page-title',
   templateUrl: './page-title.component.html',
 })
 export class PageTitleComponent {
-  @Input() position: PageTitlePosition = 'left';
-  @Input() label: string = DEFAULT_LABEL;
-  @Input() tooltip: string | undefined;
+  @Input() position: PageTitlePosition = 'center';
+  @Input() icon: Optional<string>;
+  @Input() reference: Optional<string>;
+  @Input() label: Optional<string>;
+  @Input() tooltip: Optional<string>;
 }
